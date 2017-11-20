d5ea1e527b09faff2a2f9e203dfaebe2076b75b6
KeyGenQt <keygenqt@gmail.com>
2017-11-20 08:52:51 +0300

    first commit

---
+++ ListView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace keygenqt\lazyLoad;
+
+class ListView extends \yii\widgets\ListView
+{
+//    public function run()
+//    {
+//        return $this->getView()->render('@keygenqt/imageAjax/views/view', ['widget' => $this]);
+//    }
+}
